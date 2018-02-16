9e26dd7432ad3a71f19cdd643cd11d54cc77671f
Daniel Han Herzegh <danhe178@student.liu.se>
2018-02-16 14:52:14 +0100

    4.2 godkänd

--- lab2.m
+++ lab2.m
@@ -10,7 +10,7 @@ x2=sin(2*pi*f2*t);
 
 s1 = fs/f1;
 s2 = fs/f2;
-
+%{
 plot(t(1:s1*5+1), x1(1:s1*5+1), 'b-',t(1:s1*5+1), x1(1:s1*5+1), 'rx');
 hold;
 plot(t(1:s1*5+1), x2(1:s1*5+1), 'b-',t(1:s1*5+1), x2(1:s1*5+1), 'rx');
@@ -18,3 +18,20 @@ figure;
 hist(x1, 100);
 figure;
 hist(x2, 100);
+%}
+
+f=fs/N*n;
+
+%{
+plot(f, abs(fft(x1)));
+%axis([7950 8050 0 1000]); 
+figure;
+plot(f, abs(fft(x2)));
+%axis([7950 8050 0 1000]); 
+
+%logaritmisk skala (dB)
+plot(f, db(abs(fft(x1))));
+figure;
+plot(f, db(abs(fft(x2))));
+%}
+
