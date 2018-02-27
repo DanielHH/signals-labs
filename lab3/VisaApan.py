1301b7cc6bbff28dd99bd0ae578a5ede2a4f340c
Daniel Herzegh <danhe178@student.liu.se>
2018-02-27 19:11:12 +0100

    finished question2

--- lab3/VisaApan.py
+++ lab3/VisaApan.py
@@ -1,8 +1,14 @@
 exec(open('preambel.py').read())
 Im = np.double(misc.imread('baboon.TIF'))
+
 plt.subplot(121)
 plt.imshow(Im,'gray',clim=(0,255))
-plt.title('apa')
+plt.title('lågkontrast-apa')
+plt.colorbar()
+
+plt.subplot(122)
+plt.imshow(Im,'gray',clim=(50,200)) # fråga 2
+plt.title('högkontrast-apa')
 plt.colorbar()
 
 def on_press(event):
@@ -18,3 +24,22 @@ plt.show()
 #print(np.max(Im)) # 207.0
 
 ##### fraga 2
+"""
+ LÄGRE KONTRAST
+ värde: 64
+ x-koord: 62.6613
+ y-koord: 15.1237
+ 
+ HÖGRE KONTRAST
+ värde: 64
+ x-koord: 62.7473
+ y-koord: 15.1237
+"""
+
+
+
+
+### 4.1 Förberedelseuppgift: Viktat medelvärdesbildande filter (lågpassfilter)
+
+
+
