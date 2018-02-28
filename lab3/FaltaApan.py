405cfb9ce12f4f6ae856209062aad30927fd6722
Daniel Herzegh <danhe178@student.liu.se>
2018-02-28 09:32:03 +0100

    Finished guestion7 fo realz

--- lab3/FaltaApan.py
+++ lab3/FaltaApan.py
@@ -5,7 +5,7 @@ plt.subplot(121)
 plt.imshow(Im,'gray',clim=(0,255))
 plt.title('apa')
 plt.colorbar()
-plt.show()
+#plt.show()
 
 #### Fråga 6
 
@@ -22,7 +22,17 @@ b2 = np.convolve(b,b).reshape(1,-1)
 print(b2)
 print('\n')
 aver = np.kron(b2,b2.T)
-print(aver)
+print(aver) # Kärnan
+
+# Kärnan ser korrekt ut
+
+
+Imaver = signal.convolve2d(Im,aver,'same')
+plt.subplot(122)
+plt.imshow(Imaver,'gray',clim=(0,255))
+plt.title('apa')
+plt.colorbar()
+plt.show()
 
 #SVAR
-# Kärnan ser korrekt ut
+#Det blir suddigare
