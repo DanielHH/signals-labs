0b619f57f41abb553114a1ab61001a934422bf7e
Richard Almgren Friberg <rical803@student.liu.se>
2018-03-01 17:21:33 +0100

    fraga 2 klar

---
+++ lab4/preambel.py
@@ -0,0 +1,5 @@
+import numpy as np
+from scipy import signal, misc
+from matplotlib import pyplot as plt
+plt.rcParams['image.interpolation'] = 'nearest'
+import jpeglab as jl
---
+++ lab4/prep.py
@@ -0,0 +1,29 @@
+exec(open('preambel.py').read())
+
+im1 = misc.imread('image1.png')
+im1r = im1[:,:,0]
+im1g = im1[:,:,1]
+im1b = im1[:,:,2]
+plt.figure(1), plt.imshow(im1)
+#plt.figure(2), plt.imshow(im1g,'gray')
+#plt.figure(3), plt.imshow(im1b,'gray')
+#plt.show()
+
+### FRAGA 1
+#SVAR
+# Gul: lika blandning rött och grönt
+# Cyan: lika blått ocht grönt
+# Magenta: lika blått och rött
+# White: lika max rgb
+# SVart: 0 rgb
+
+y, cb, cr = jl.rgb2ycbcr(im1)
+
+plt.figure(2), plt.imshow(y, 'gray', clim=(0, 255))
+plt.figure(3), plt.imshow(cb, 'gray')
+plt.figure(4), plt.imshow(cr, 'gray')
+plt.show()
+
+### FRAGA 2
+#SVAR
+# I luminens, figur 2 (y-kanal)
