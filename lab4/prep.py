584fd94dde2d41739c469373fddedb32d637e748
Richard Almgren Friberg <rical803@student.liu.se>
2018-03-02 09:17:23 +0100

    bbpReduction func added

--- lab4/prep.py
+++ lab4/prep.py
@@ -19,9 +19,9 @@ plt.figure(1), plt.imshow(im1)
 
 y, cb, cr = jl.rgb2ycbcr(im1)
 
-plt.figure(2), plt.imshow(y, 'gray', clim=(0, 255))
-plt.figure(3), plt.imshow(cb, 'gray')
-plt.figure(4), plt.imshow(cr, 'gray')
+#lt.figure(2), plt.imshow(y, 'gray', clim=(0, 255))
+#plt.figure(3), plt.imshow(cb, 'gray')
+#plt.figure(4), plt.imshow(cr, 'gray')
 #plt.show()
 
 ### FRAGA 2
@@ -37,3 +37,10 @@ def psnr(mse):
     return 10*np.log(255**2/mse)
 
 print(psnr(meanSquareError(im1g, im1r)))
+
+def bppReduction(img, bits_to_reduce):
+    X = 2**bits_to_reduce
+    return X*np.floor_divide(img,X)
+
+plt.figure(2), plt.imshow(bppReduction(im1, 4))
+plt.show()
